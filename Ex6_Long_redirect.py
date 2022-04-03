4ead55c6586ec387ca95e6a0c6d15167cfa3c275
Aprelika <83813203+Aprelika@users.noreply.github.com>
2022-04-03 17:08:06 +0300

    Update Ex6_Long_redirect.py

--- Ex6_Long_redirect.py
+++ Ex6_Long_redirect.py
@@ -5,4 +5,4 @@ for r in respons.history:
     print(r.status_code, r.url)
 else:
     allow_responses=False
-    print("Итоговый редерект: ", r.status_code, r.url
+    print("Итоговый редерект: ", r.status_code, r.url)
