7a3c7f830a8d105a5297f404a835bdfdfbb29d78
Aprelika <83813203+Aprelika@users.noreply.github.com>
2022-04-05 15:12:51 +0300

    Delete Ex6_Long_redirect.py

--- Ex6_Long_redirect.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-respons = requests.get("https://playground.learnqa.ru/api/long_redirect")
-for r in respons.history:
-    print(r.status_code, r.url)
-else:
-    allow_responses=False
-    print("Итоговый редерект: ", r.status_code, r.url)
