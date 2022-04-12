b0991b52b785b601d3e43dd5e06e051a8c62dcd8
Natali <_kapriza_@mail.ru>
2022-04-12 13:09:10 +0300

    Add new

--- test_cookie.py
+++ test_cookie.py
@@ -1,5 +1,14 @@
 import requests
 
+class TestCookie:
+      def test_cookie(self):
 response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
-print(response.text, response.status_code)
-print(response.cookies)
+cookie_value = dict(response.cookies) #
+for key in cookie_value:
+    print(cookie_value[key])
+else:
+    print(key)
+assert "HomeWork" in cookie_value, "These cookies are not in the response"
+expected_respose_key = key
+expected_respose_value = cookie_value[key]
+assert expected_respose_key == expected_respose_value, "Cookies is not correct"
