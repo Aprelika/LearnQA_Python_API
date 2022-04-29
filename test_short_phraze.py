fb812d29d52cb243fca8c580e4f36e8ff29cd949
Natali <_kapriza_@mail.ru>
2022-04-29 12:35:41 +0300

    add test headers

--- test_short_phraze.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-class TestShortPhrase:
-    def test_short_phrase(self):
-        phrase = input("Введите любую фразу: ")
-        expected = 15
-        assert len(phrase) <= expected, "Ошибка фраза больше 15 символов"
