6637ba967ebd2fd48bd0168bbf82bb05c50fd6d1
Natali <_kapriza_@mail.ru>
2022-05-14 12:43:46 +0300

    add test test_user_details_auth_user

--- lib/assertions.py
+++ lib/assertions.py
@@ -9,7 +9,7 @@ class Assertions:
         except json.JSONDecodeError:
             assert False, f"Response is not JSON format. Response text is '{response.text}'"
 
-        assert name in response_as_dict,f"Response JSON doesn't have key '{name}'"
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
 
     @staticmethod
@@ -20,6 +20,25 @@ class Assertions:
             assert False, f"Response is not JSON format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it's present"
 
     @staticmethod
     def assert_code_status(response: Response, expected_status_code):
